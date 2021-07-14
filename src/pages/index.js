11a164a9ea9ecb0b2951e8a7ea71295558f61f2b
Sean Gu <sean@ipwe.com>
2021-07-14 09:06:02 +0800

    commit on 'main'

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
 import React from "react"
+import Layout from "./components/Layout"
+import * as style from "./styles/home.module.css"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <Layout>
+      <div className={style.section}>
+        <h1>Hello world!</h1>
+        <img src="/seangu.jpg" alt="Sean Gu" />
+      </div>
+    </Layout>
+  )
 }
